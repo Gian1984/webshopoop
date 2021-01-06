1a6716a68ef2606bfc6aa3860dcf3449b76f089a
“Gian1984” <gl.tiengo@gmail.com>
2021-01-06 16:47:12 +0100

    fixing navbar

--- classes/prods.class.php
+++ classes/prods.class.php
@@ -13,6 +13,16 @@ class Prods extends Dbh {
         return $result;    // i put all the the information of the fetch indise result to be able to send it to the users.view.php 
     }
 
+    protected function getProds($prod){
+
+        $sql = "SELECT * FROM products WHERE types =  ?"; //selecting from database
+        $stmt = $this->connect()->prepare($sql); //istantiate a new statement and utilize $this coz i refer to database class and ask to the database to prepare it
+        $stmt->execute([$prod]);
+
+        $result = $stmt->fetchAll();
+        return $result;    // i put all the the information of the fetch indise result to be able to send it to the users.view.php 
+    }
+
 
 
 
